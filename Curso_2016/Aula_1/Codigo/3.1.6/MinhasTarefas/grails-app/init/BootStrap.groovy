691b156aa9b7151377b20cf680ba01e86848a31a
Bruno <blopes@ufscar.br>
2016-05-11 11:39:51 -0300

    Alterar BootStrap.groovy para inserir listas de tarefas

--- Curso_2016/Aula_1/Codigo/3.1.6/MinhasTarefas/grails-app/init/BootStrap.groovy
+++ Curso_2016/Aula_1/Codigo/3.1.6/MinhasTarefas/grails-app/init/BootStrap.groovy
@@ -1,7 +1,15 @@
+import br.ufscar.minhasTarefas.ListaTarefa
+
 class BootStrap {
 
     def init = { servletContext ->
+        15.times { i ->
+            ListaTarefa listaTarefa = new ListaTarefa(nome: "Lista ${i + 1}").save(failOnError: true)
+        }
+
     }
     def destroy = {
     }
 }
+
+
