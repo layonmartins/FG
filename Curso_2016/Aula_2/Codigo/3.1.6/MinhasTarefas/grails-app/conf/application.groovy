07115fc8cb128a89793bcd53d03af0d1119cc784
Bruno <blopes@ufscar.br>
2016-05-13 08:49:53 -0300

    Adicionar plugin spring security ui e liberar acesso para permissao ROLE_ADMIN

--- Curso_2016/Aula_2/Codigo/3.1.6/MinhasTarefas/grails-app/conf/application.groovy
+++ Curso_2016/Aula_2/Codigo/3.1.6/MinhasTarefas/grails-app/conf/application.groovy
@@ -14,7 +14,10 @@ grails.plugin.springsecurity.controllerAnnotations.staticRules = [
 	[pattern: '/**/js/**',       access: ['permitAll']],
 	[pattern: '/**/css/**',      access: ['permitAll']],
 	[pattern: '/**/images/**',   access: ['permitAll']],
-	[pattern: '/**/favicon.ico', access: ['permitAll']]
+	[pattern: '/**/favicon.ico', access: ['permitAll']],
+	[pattern: '/role/**',        access: ['ROLE_ADMIN']],
+	[pattern: '/user/**',        access: ['ROLE_ADMIN']],
+	[pattern: '/securityInfo/**',access: ['ROLE_ADMIN']]
 ]
 
 grails.plugin.springsecurity.filterChain.chainMap = [
